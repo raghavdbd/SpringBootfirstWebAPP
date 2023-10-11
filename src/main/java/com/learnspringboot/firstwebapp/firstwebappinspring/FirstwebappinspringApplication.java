eaa5b5e4979c50ec2e5dce9d979b27eca8f2cff2
raghav goyal <raghavgoyaldbd4782gmail.com>
2023-10-11 12:27:37 +0530

    First Comit

---
+++ src/main/java/com/learnspringboot/firstwebapp/firstwebappinspring/FirstwebappinspringApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.firstwebapp.firstwebappinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebappinspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwebappinspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringboot/firstwebapp/firstwebappinspring/FirstwebappinspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.firstwebapp.firstwebappinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwebappinspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
